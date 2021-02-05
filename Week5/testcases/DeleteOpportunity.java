a0882d472c75923083e84f165775359bc7fc3fd6
Roopamanickam <58107471+Roopamanickam@users.noreply.github.com>
2021-02-05 14:48:55 +0530

    DeleteOpportunity
    Testcase DeleteOpportunity

---
+++ Week5/testcases/DeleteOpportunity.java
@@ -0,0 +1,37 @@
+package testcases;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import base.ProjectSpecificMethods;
+import pages.LoginPage;
+
+public class DeleteOpportunity extends ProjectSpecificMethods {
+	
+	@BeforeTest
+	public void SetFile() {
+		excelFileName ="CreateOpportunity";
+	}
+
+	@Test(dataProvider="fetchData" , dependsOnMethods ="testcases.EditOpportunity.editOpportunity")
+	public void deleteOpp(String userName, String password, String oppName, String amt ) throws InterruptedException {
+		
+		new LoginPage(driver)
+		.enterUsername(userName)
+		.enterPassword(password)
+		.clickLoginButton()
+		.appLauncher()
+		.viewAll()
+		.sales()
+		.opportunityForEdit()
+		.search(oppName)
+		.enter()
+		.clkDdSearchResultForDelete()
+		.clkDeleteOpp()
+		.clkOKDelete()
+		.confirmDelete(oppName);
+		//.getID();
+		
+
+	}
+}
