e17294dd7ab9c5770f9f3fc685be897dc2d3e30c
Roopamanickam <58107471+Roopamanickam@users.noreply.github.com>
2021-02-07 15:05:04 +0530

    Update DeleteOpp.java
    Updated the getID() function

--- Week5/pages/DeleteOpp.java
+++ Week5/pages/DeleteOpp.java
@@ -44,22 +44,18 @@ public class DeleteOpp extends ProjectSpecificMethods {
 		return this;
 	}
 
-	/*public DeleteOpp getID() {
+	public DeleteOpp getID() {
 		System.out.println("===***^^^Orginal OppID To verify With*****^^^== "+oppId);		
 		WebElement table = driver.findElementByXPath("//table[@data-aura-class='uiVirtualDataTable']");
 		List<WebElement> totRows = driver.findElements(By.tagName("tr"));
 		int rowCount = totRows.size();
 		System.out.println("===Row Count===:"+rowCount);
 
-		for (int i = 0; i < rowCount; i++) {
+		for (int i = 1; i <= rowCount; i++) {
 			WebElement firstRow = totRows.get(i);
 
-			List<WebElement> allCol = firstRow.findElements(By.tagName("th"));
-			int colCount = allCol.size();
 			WebElement verifyIdWebEle = driver.findElementByXPath("(//table[@data-aura-class='uiVirtualDataTable']/tbody/tr)["+i+"]/th//a");
-			String oppIdToVerify = verifyIdWebEle.getAttribute("data-recordid");
-			WebElement oppIdWbEle = allCol.get(0);
-			String oppIDchk = oppIdWbEle.getText();
+			String oppIdToVerify = verifyIdWebEle.getAttribute("data-recordid");	
 
 			System.out.println("===OppID "+i+" from Table=="+oppIdToVerify);
 			
@@ -71,5 +67,5 @@ public class DeleteOpp extends ProjectSpecificMethods {
 		}
 		System.out.println("Opportunity  Deleted Successfully");
 		return this;
-	}*/
+	}
 }
