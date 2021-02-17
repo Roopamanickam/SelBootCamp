61696c3490194cc7c5e25500aa064010374bc869
Roopamanickam <58107471+Roopamanickam@users.noreply.github.com>
2021-02-17 12:02:33 +0530

    Update LoginLandingPage.java
    TO date not able to click using calendar

--- Assesment2/LoginLandingPage.java
+++ Assesment2/LoginLandingPage.java
@@ -5,9 +5,13 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.imageio.ImageIO;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
@@ -18,22 +22,22 @@ import cucumber.api.java.en.When;
 
 public class LoginLandingPage extends ParentClass{
 
-	@Given("Enter username (.*)")
+	@Given("Enter the username (.*)")
 	public void enterUsername(String username) {
 		driver.findElementById("username").sendKeys(username);
 	}
 	
-	@Given("Enter password (.*)")
+	@Given("Enter the password (.*)")
 	public void enterPassword(String password) {
 		driver.findElementById("password").sendKeys(password);
 	}
 	
-	@When("Click Login button")
+	@When("Click the Login button")
 	public void clickLogin() {
 		driver.findElementById("Login").click();
 	}
 	
-	@Then("Landingpage should be displayed")
+	@Then("The Landing page should be displayed")
 	public void verifyLandingPage() {
 		String pageTitle = driver.getTitle();
 		if(pageTitle.contains("Home")) {
@@ -41,13 +45,13 @@ public class LoginLandingPage extends ParentClass{
 		}
 	}
 	
-	@When("Click AppLauncher")
+	@When("Click Application Launcher")
 	public void clickAppLauncher() {
 		WebElement menu = driver.findElementByXPath("//div[@class='slds-icon-waffle']");
 		menu.click();
 	}
 	
-	@When("Click View All")
+	@When("Click the View All")
 	public void clickViewAll() {
 		WebElement viewAll = driver.findElementByXPath("//button[text()='View All']");
 		viewAll.click();
@@ -66,21 +70,27 @@ public class LoginLandingPage extends ParentClass{
 		executor.executeScript("arguments[0].click();", oppdpdwm);
 	}
 	
-	@And("And Click New Reports SFClassic")
-	public void clkNewReportSFClassic() {
-		WebElement newRptSfClassic  = driver.findElementByXPath("//a[@title='New Report (Salesforce Classic)']");
+	@And("Click New Reports SFClassic")
+	public void clkNewReportSFClassic() throws InterruptedException {
+		WebElement newRptSfClassic  = driver.findElementByXPath("//div[@title='New Report (Salesforce Classic)']");
 		newRptSfClassic.click();
+		Thread.sleep(3000);
 	}
 	
 	@And("Click on Leads")
 	public void clkLeads() {
-		driver.findElementByXPath("(//span[text()='Leads'])[1]").click();
+		driver.switchTo().frame(0);
+		driver.findElementByXPath("(//form[@id='thePage:dummyForm']/table//td[1]//div/ul[@class='x-tree-root-ct x-tree-lines']/div/li)[4]").click();
+		System.out.println("Leads clicked");
 	}
 	
 	@Then("Lead Report Image should be displayed")
 	public void verifyLeadReport() {
-		WebElement leadRpt  = driver.findElementByXPath("//img[@src=/img/rptleadlist.gif]");
 		
+		WebElement leadRpt  = driver.findElementByXPath("//form[@id='thePage:dummyForm']/table//td[2]/div[@class='previewBlock']");
+	
+		System.out.println("inside Leads img display block");
+		//img[@src=/img/rptleadlist.gif]
 				if(leadRpt.isDisplayed()) {
 					boolean leadRptVerify = true;
 					System.out.println("Lead Report Image is displayed");
@@ -89,43 +99,82 @@ public class LoginLandingPage extends ParentClass{
 	
 	@When("Download the LeadReport")
 	public void dwnldLeadRpt() throws MalformedURLException, IOException {
-		WebElement imgElement = driver.findElementByXPath("//img[@src=/img/rptleadlist.gif]");
+		WebElement imgElement = driver.findElementByXPath("//img[@id='thePage:dummyForm:report_img']");
 		String src = imgElement.getAttribute("src");
 		BufferedImage bufferedImage = ImageIO.read(new URL(src));
 		File outputfile = new File("saved.png");
 		ImageIO.write(bufferedImage, "png", outputfile);
-		
+
 	}
 	
 	@And("Click Create button")
-	public void clkCreateBtn() {
+	public void clkCreateBtn() throws InterruptedException {
 		driver.findElementByXPath(" //input[@value='Create']").click();
+		System.out.println("Create button clicked");
+		Thread.sleep(4000);
 	}
 	
-	/*@When("Select Range as All Time")
-	public void selectRangeAsAllTime() {
-	    // Write code here that turns the phrase above into concrete actions
-	    
+	@When("Select Range as All Time")
+	public void selectRangeAsAllTime() throws InterruptedException {
+	WebElement rangeClk = driver.findElementByXPath("(//table[@class='x-table-layout'])[3]//tr[2]/td[2]//div/form//input[@name='duration']");
+	rangeClk.click();
+	Thread.sleep(3000);
+	/*JavascriptExecutor executor = (JavascriptExecutor)driver;
+	executor.executeScript("arguments[0].click();", rangeClk);*/
+	System.out.println("All dropdown clicked");
+	//rangeClk.click();
+	driver.findElementByXPath("//div[@class='x-combo-list-inner']/div[text()='All Time']").click();
+	System.out.println("All Time clicked");	    
 	}
-
+	
 	@When("Select From date as todays date")
 	public void selectFromDateAsTodaysDate() {
-	    // Write code here that turns the phrase above into concrete actions
-	    
+	   driver.findElementByXPath("((//table[@class='x-table-layout'])[3]//tr[2]/td[2]//div/form//img[@class='x-form-trigger x-form-date-trigger'])[1]").click();
+	   System.out.println("todays dd clicked");
+	    driver.findElementByXPath("//button[text()='Today']").click();
+	    System.out.println("todays date clicked");
+	}
+	
+	@When("Select ToDate as PlusFive days From Today")
+	public void selectToDateAsPlusDaysFromToday() throws InterruptedException {
+	//TO choose tmrw's date as StartDate
+		 	//driver.findElementByXPath("((//table[@class='x-table-layout'])[3]//tr[2]/td[2]//div/form//img[@class='x-form-trigger x-form-date-trigger'])[2]").click();
+		driver.findElementByXPath("//input[@name='endDate']/following-sibling::img[@class='x-form-trigger x-form-date-trigger']").click();
+		System.out.println("==========Date dropdown Clicked=======");
+		 	Thread.sleep(3000);
+					//Set DateFormat
+					DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+					 
+					 //get current date time with Date() and add a day to it
+					 Date rawstartDate = new Date(new Date().getTime() + (5*24*60*60*1000));
+					 
+					 // Now format the date
+					 String startDate = dateFormat.format(rawstartDate);
+					 String[] spltDate = startDate.split("/");
+					 
+					 System.out.println("@@@@@@@@@@@@@  "+startDate);	
+					 System.out.println("***"+spltDate[1]);
+										 
+					 String datXpath="//a[@class='x-date-date']//span[text()='"+startDate+"']";
+					 
+					 System.out.println("========"+datXpath);
+					
+					 WebElement strtDateClk = driver.findElement(By.xpath(datXpath));
+					 strtDateClk.click();	         
+				    
 	}
 
-	@When("Select ToDate as Plus{int} days From Today")
-	public void selectToDateAsPlusDaysFromToday(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new cucumber.api.PendingException();
-	}
-
+	
 	@When("Click Preview as Tabular Format")
 	public void clickPreviewAsTabularFormat() {
-	    // Write code here that turns the phrase above into concrete actions
-	   
+	WebElement tabFormat = driver.findElementByXPath("//table[@id='reportFormatMink']//button");
+	String tabFormatTxt = tabFormat.getText();
+	System.out.println("==="+tabFormatTxt);
+	 	   if(tabFormatTxt.contentEquals("Tabular Format")) {
+	 		   System.out.println("Tabular format success");
+	 	   }
 	}
-
+	/*
 	@When("Get the List of Billing State\\/Province")
 	public void getTheListOfBillingStateProvince() {
 	    // Write code here that turns the phrase above into concrete actions
